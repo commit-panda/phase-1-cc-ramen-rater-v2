1862e79418d3bda9326be9b37e6dac68dc38a79d
commit-panda <sjmanyarkiy@gmail.com>
2025-10-21 12:19:54 +0300

    added submit event listener

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
-  document.getElementById('new-ramen').addEventListener("submit", (e) => {
-    e.preventDefault
-    const newRamen = 
-
-  })
+  
 
 };
 
 const addSubmitListener = () => {
   // Add code
+  document.getElementById('new-ramen').addEventListener("submit", (e) => {
+    e.preventDefault
+    const name = document.getElementById("new-name")
+    const restaurant = document.getElementById("new-restaurant")
+    const image = document.getElementById("new-image")
+    const rating = document.getElementById("new-rating") 
+    const comment = document.getElementById("new-comment")
+
+  })
 }
 
 const displayRamens = () => {
   // Add code
+  
+
 };
 
 const main = () => {
