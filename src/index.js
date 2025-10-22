b5f29df2ba5d31fa89e2a22f4af417ea3628f9d1
commit-panda <sjmanyarkiy@gmail.com>
2025-10-22 13:43:28 +0300

    completed handleClick function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
 // index.js
 
+const baseUrl = 'http://localhost:3000/ramens'
+
+
+
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
-  
 
-};
-
-const addSubmitListener = () => {
-  // Add code
-  document.getElementById('new-ramen').addEventListener("submit", (e) => {
-    e.preventDefault
     const name = document.getElementById("new-name")
     const restaurant = document.getElementById("new-restaurant")
     const image = document.getElementById("new-image")
     const rating = document.getElementById("new-rating") 
     const comment = document.getElementById("new-comment")
 
-  })
+    name.textContent = ramen.name
+    restaurant.textContent = ramen.restaurant
+    image.src = ramen.image
+    rating.textContent = ramen.rating
+    comment.textContent = ramen.comment
+
+}
+
+const addSubmitListener = () => {
+  // Add code
+  document.getElementById('new-ramen').addEventListener("submit", handleClick())
+
 }
 
 const displayRamens = () => {
   // Add code
-  document.getElementById("ra")
+  fetch(`${baseUrl}`)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach((ramen) => {
+       document.getElementById('ramen-menu').innerHTML = `
+    <img src=${data.image}>
+    `
+    })
+   
+  })
+
+  
 
 };
 
